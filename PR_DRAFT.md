94b8a20f60b4e3d9c74d49b31b5ed893e2096524
Jonah <tgtechie01@gmail.com>
2022-05-12 22:32:49 -0400

    init commit - add typing_ module - start of draft pr

---
+++ PR_DRAFT.md
@@ -0,0 +1,82 @@
+Draft Pull Request `__class_getitem__`
+(this may be better as two PRs)
+(start below)
+
+---
+<!-- The next line is the PR title-->
+### Improve cpython `typing` compatibility with `__class_getitem__`
+
+This PR adds the `__class_getitem__` method to classes in-order to better support cpython like type-hinting. It also has two other typing related features.
+
+Circuitpython is largely compatible with the cpython like typing because it supports type-hint syntax without executing it. 
+There are three exceptions to that.
+
+However, the protocol / generic typing features require some non-type-hint syntax since `Generic` and `Protocol` need to be passed to a class's bases.
+##### example
+```python
+T = TypeVar('T')
+class Wrapper(Generic[T]):
+    wrapped: T
+    ...
+class Peripheral(Protocol[T]):
+  def deinit(self) -> None:
+    ...
+```
+
+#### Why
+
+
+### Rationale
+
+## Circuitpython Core Support
+> :warning: TODO(tg-techie, before pr): remove the `|` mention if not added.
+
+This PR adds 3 toggle-able features to the core:
+#### `__class_getitem__` from [Pep 560](https://peps.python.org/pep-0560/#class-getitem) as dicussed above.
+#### [Pep 585](https://peps.python.org/pep-0585/) like builin generic `[]` operator on type  so it return itself
+  ex:`dict[str, int] is dict # True`
+#### [Pep 604 like](https://peps.python.org/pep-0604/) 
+  This adds the `|` operator on type so it returns `object` 
+  ex: `(str | None) is object # True`
+
+## Implementing the `typing` module
+
+TODO: add link to runtime 
+
+Only add the typing feature that have to be used outside of type-hints.
+
+Should it be implemented in python-land or in the core?
+
+If it is implemented in the core? 
+
+The answer may be either. 
+
+Working minimain
+
+no way to cut-off the **class_getitem** functionality **without** auto-generating an extra class. (If Foo[a, b, c] did not have a typevar or paramspec in a, b, c)>> That class would manually raise an error when **class_getitem** is called on it.
+  > In the typing\_.py implementation in python-land, circuitpython implementation this can be turned on/off by setting the GENERATE_NONGENERIC_BASES attribute on the `Generic` class.
+
+> :warning: TODO: add example here?
+
+## Questions I have / Feedback
+
+- Should the `typing` module be in python-land or the core
+- From some looking around, 
+
+
+
+### Background
+
+The `__class_getitem__` method is a special method that was added in cpython 3.7 used to supper the `[]` operator on classes without using metaclasses. 
+
+### Misc Other Thoughts
+- For later discussion: Use of `TYPE_CHECKING` or `try:...` or making a new circuitpython-ism (maybe `from micropython import annotation_only` like `TYPE_CHECKING`)
+
+### Considered alternatives:
+
+#### Special Syntax
+Add ignoring `[...]` in class definitions 
+
+#### type `[]` without `__class_getitem__`
+- add the `[]`` operator to all type object to take __anything__ and return the type it was called on. See a [rudimentary implementation here](https://github.com/TG-Techie/circuitpython/blob/5a08692980f8568d6ef05b8d207a1114080aaf86/py/objtype.c#L1153-L1169)
+  > This would have a smaller code footprint, however it would make every type everywhere return itself from the `[]` operator. `SomeType[1, 2, 3] is SomeType # True`
---
+++ typing_.py
@@ -0,0 +1,194 @@
+# LICENSE: MIT
+#
+# Copyright (c) 2022, Jonah "Jay" Y-M (@TG-Techie)
+#
+# See repo for license text.
+
+"""
+`typing` (circuitpython_stdlib_typing)
+=======================
+
+A prototype module for circuitpython to replace the `typing` module in the cpython standard library.
+This aims to add compatibility only for features that cannot go in type-hints.
+
+Please review the assoicated README.md / Pull request for more information. 
+
+This below code works in-conjuntion with changes made to circuitpython:
+see preliminary changes in `py/objtype.c` file here https://github.com/TG-Techie/circuitpython/tree/add_class_getitem
+"""
+
+from __future__ import annotations
+
+
+TYPE_CHECKING: Literal[True] = False  # type: ignore
+
+
+class Protocol:
+    def __new__(cls, *_, **__):
+        assert cls is not Protocol, "Protocol cannot be instantiated"
+        return object.__new__(cls)
+
+    def __class_getitem__(cls, params: type) -> type[Protocol]:
+        _check_are_typeparams(params)
+        return cls
+
+
+class Generic:
+
+    GENERATE_NONGENERIC_BASES = False
+
+    def __new__(cls, *_, **__):
+        assert cls is not Generic, "Generic(...) cannot be instantiated"
+        return object.__new__(cls)
+
+    def __class_getitem__(cls, params):
+        assert not isinstance(
+            params, slice
+        ), "slicing now allowed in type parameters, ex `Generic[T:S]`"
+        # check all the inputs are of the correct types
+        typeparams = _check_are_typeparams(params)
+        # generic only allows typevar like
+        if cls is Generic:
+            assert all(
+                isinstance(tp, _TypeVarLike) for tp in typeparams
+            ), "Generic[...] only supports TypeVar, ParamSpec, etc type parameters"
+        elif cls.GENERATE_NONGENERIC_BASES:
+            # generate a non-generic base when the there are no TypeVar like items in the type params
+            # assume strings are forwards refs and not generic
+            # TDOD: revisit assumption
+            if all(not isinstance(t, _TypeVarLike) for t in typeparams):
+                if wrapper_cls := getattr(cls, "_nongeneric_wrapper_cls", None):
+                    wrapper_cls = cls._nongeneric_wrapper_cls = type(  # type: ignore
+                        cls.__name__,
+                        (wrapper_cls,),
+                        {"__class_getitem__": _nongeneric_class_getitem},
+                    )
+                    wrapper_cls.__module__ = cls.__module__
+                    wrapper_cls.__qualname__ = cls.__qualname__
+                    wrapper._generic_base_cls = cls  # type: ignore
+                return wrapper_cls
+        return cls
+
+
+class _TypeVarLike:
+    # # these attrs are skipped but would be present in cpython
+    # __name__: str # the name of the type variable, the first argument to TypeVar
+    # __covariant__: bool
+    # __contravariant__: bool
+    # __bound__: <type param> | None
+    # __module__: str | None
+
+    # _TypeVarLike subclseses only have one instance, store related values in the class
+    __single_instance: _TypeVarLike  # auto-created
+    __repr_str: str  # defined in the subclass
+
+    def __new__(
+        cls,
+        name: str,
+        *,
+        bound=None,
+        contravariant=False,
+        covariant=False,
+    ):
+        # intentiaonally not using super()
+        assert (
+            cls is not _TypeVarLike
+        ), "_TypeVarLike(...) called, it should not be instantiated"
+
+        assert not (
+            covariant and contravariant
+        ), "Bivariant types are not supported, cannot be both covariant and contravariant"
+        # skipped # self.__constraints__ = tuple of constraints given
+        # skipped #  self.__module__ = callsite_globals['__name__] exlcuding 'typing'
+
+        # TODO(tg-techie): should there be one per name, and store them in a dict?
+        # only make one instance per class to save memory
+        if (self := getattr(cls, "__single_instance", None)) is None:
+            self = cls.__single_instance = object.__new__(cls)
+            # if making a new one, init the realted values in the class __dict__
+            assert hasattr(cls, "__repr_str")  # define these in the subclasses
+
+        return self
+
+    def __repr__(self) -> str:
+        return self.__repr_str
+
+
+class TypeVar(_TypeVarLike):
+    # # additional skipped field
+    # __constraints__: tuple[<type parmas ...>] | tuple[()]
+
+    __repr_str = "<TypeVar>"
+
+    def __new__(
+        cls,
+        name: str,
+        *constraints,
+        bound=None,
+        contravariant=False,
+        covariant=False,
+    ):
+        assert cls is TypeVar, "TypeVar cannot be subclassed"
+
+        assert not (
+            constraints and bound is not None
+        ), "Constraints cannot be combined with bound=..."
+
+        assert (
+            not constraints or len(constraints) > 1
+        ), "A single constraint is not allowed"
+
+        _check_are_typeparams(constraints)
+
+        return _TypeVarLike.__new__(
+            cls,
+            name=name,
+            bound=bound,
+            contravariant=contravariant,
+            covariant=covariant,
+        )
+
+
+# the related `Concatenate[...]` type is only used in annotations so does not need to exist at runtime
+class ParamSpec(_TypeVarLike):
+
+    __repr_str = "<ParamSpec>"
+
+    def __new__(cls, name, *, bound=None, covariant=False, contravariant=False):
+        assert cls is ParamSpec, "ParamSpec cannot be subclassed"
+        return _TypeVarLike.__new__(
+            cls,
+            name=name,
+            bound=bound,
+            covariant=covariant,
+            contravariant=contravariant,
+        )
+
+
+_allowed_param_types = (type, str, _TypeVarLike)
+
+
+def _check_are_typeparams(params) -> tuple:
+    global _allowed_param_types
+    typeparams = params if params.__class__ is tuple else (params,)
+    assert all(
+        isinstance(tp, _allowed_param_types) for tp in typeparams
+    ), " type parameters must be types, ForwardRefs (strings), TypeVars, or ParamSpecs"
+    return typeparams
+
+
+def _nongeneric_class_getitem(cls: type[Generic], _params):
+    raise TypeError(
+        f"{cls.__name__} is not generic and cannot be used with type parameters"
+    )
+
+
+# check that __class_getitem__ is supported,
+try:
+    Generic[TypeVar("T")]
+except TypeError:
+    raise RuntimeError(
+        " Class typing parameters (ex: Generic[T]) not supported, "
+        "you may need to use a different version of circuitpython."
+        "(see: https://github.com/tg-techie/CircuitPython_Typing)"
+    )
